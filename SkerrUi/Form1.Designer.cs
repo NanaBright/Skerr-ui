1c8fb00bd5fe00d98953a1bc1909f8ffc9eede9d
[NB-Skerr] <[nanabryte.nb@gmail.com]>
2022-09-29 23:10:44 +0000

    buttons

---
+++ SkerrUi/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace SkerrUi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttons1 = new SkerrUi.Ui_Stuff.buttons();
+            this.buttons2 = new SkerrUi.Ui_Stuff.buttons();
+            this.buttons3 = new SkerrUi.Ui_Stuff.buttons();
+            this.buttons4 = new SkerrUi.Ui_Stuff.buttons();
+            this.buttons5 = new SkerrUi.Ui_Stuff.buttons();
+            this.buttons6 = new SkerrUi.Ui_Stuff.buttons();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttons1
+            // 
+            this.buttons1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.buttons1.BorderRadius = 0;
+            this.buttons1.BorderSize = 0;
+            this.buttons1.FlatAppearance.BorderSize = 0;
+            this.buttons1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons1.ForeColor = System.Drawing.Color.White;
+            this.buttons1.Location = new System.Drawing.Point(267, 31);
+            this.buttons1.Name = "buttons1";
+            this.buttons1.Size = new System.Drawing.Size(188, 50);
+            this.buttons1.TabIndex = 1;
+            this.buttons1.Text = "buttons1";
+            this.buttons1.TextColor = System.Drawing.Color.White;
+            this.buttons1.UseVisualStyleBackColor = false;
+            // 
+            // buttons2
+            // 
+            this.buttons2.BackColor = System.Drawing.Color.Transparent;
+            this.buttons2.BackgroundColor = System.Drawing.Color.Transparent;
+            this.buttons2.BorderColor = System.Drawing.Color.Black;
+            this.buttons2.BorderRadius = 16;
+            this.buttons2.BorderSize = 1;
+            this.buttons2.FlatAppearance.BorderSize = 0;
+            this.buttons2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons2.ForeColor = System.Drawing.Color.Maroon;
+            this.buttons2.Location = new System.Drawing.Point(267, 123);
+            this.buttons2.Name = "buttons2";
+            this.buttons2.Size = new System.Drawing.Size(188, 50);
+            this.buttons2.TabIndex = 2;
+            this.buttons2.Text = "buttons2";
+            this.buttons2.TextColor = System.Drawing.Color.Maroon;
+            this.buttons2.UseVisualStyleBackColor = false;
+            // 
+            // buttons3
+            // 
+            this.buttons3.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.buttons3.BackgroundColor = System.Drawing.Color.MediumSeaGreen;
+            this.buttons3.BorderColor = System.Drawing.Color.Fuchsia;
+            this.buttons3.BorderRadius = 20;
+            this.buttons3.BorderSize = 1;
+            this.buttons3.FlatAppearance.BorderSize = 0;
+            this.buttons3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons3.ForeColor = System.Drawing.Color.Black;
+            this.buttons3.Location = new System.Drawing.Point(267, 212);
+            this.buttons3.Name = "buttons3";
+            this.buttons3.Size = new System.Drawing.Size(188, 50);
+            this.buttons3.TabIndex = 3;
+            this.buttons3.Text = "buttons3";
+            this.buttons3.TextColor = System.Drawing.Color.Black;
+            this.buttons3.UseVisualStyleBackColor = true;
+            // 
+            // buttons4
+            // 
+            this.buttons4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons4.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons4.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.buttons4.BorderRadius = 0;
+            this.buttons4.BorderSize = 0;
+            this.buttons4.FlatAppearance.BorderSize = 0;
+            this.buttons4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons4.ForeColor = System.Drawing.Color.White;
+            this.buttons4.Location = new System.Drawing.Point(486, 66);
+            this.buttons4.Name = "buttons4";
+            this.buttons4.Size = new System.Drawing.Size(188, 50);
+            this.buttons4.TabIndex = 4;
+            this.buttons4.Text = "buttons4";
+            this.buttons4.TextColor = System.Drawing.Color.White;
+            this.buttons4.UseVisualStyleBackColor = false;
+            // 
+            // buttons5
+            // 
+            this.buttons5.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons5.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons5.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.buttons5.BorderRadius = 0;
+            this.buttons5.BorderSize = 0;
+            this.buttons5.FlatAppearance.BorderSize = 0;
+            this.buttons5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons5.ForeColor = System.Drawing.Color.White;
+            this.buttons5.Location = new System.Drawing.Point(486, 166);
+            this.buttons5.Name = "buttons5";
+            this.buttons5.Size = new System.Drawing.Size(188, 50);
+            this.buttons5.TabIndex = 5;
+            this.buttons5.Text = "buttons5";
+            this.buttons5.TextColor = System.Drawing.Color.White;
+            this.buttons5.UseVisualStyleBackColor = false;
+            // 
+            // buttons6
+            // 
+            this.buttons6.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons6.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttons6.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.buttons6.BorderRadius = 0;
+            this.buttons6.BorderSize = 0;
+            this.buttons6.FlatAppearance.BorderSize = 0;
+            this.buttons6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttons6.ForeColor = System.Drawing.Color.White;
+            this.buttons6.Location = new System.Drawing.Point(486, 255);
+            this.buttons6.Name = "buttons6";
+            this.buttons6.Size = new System.Drawing.Size(188, 50);
+            this.buttons6.TabIndex = 6;
+            this.buttons6.Text = "buttons6";
+            this.buttons6.TextColor = System.Drawing.Color.White;
+            this.buttons6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttons6);
+            this.Controls.Add(this.buttons5);
+            this.Controls.Add(this.buttons4);
+            this.Controls.Add(this.buttons3);
+            this.Controls.Add(this.buttons2);
+            this.Controls.Add(this.buttons1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Ui_Stuff.buttons buttons1;
+        private Ui_Stuff.buttons buttons2;
+        private Ui_Stuff.buttons buttons3;
+        private Ui_Stuff.buttons buttons4;
+        private Ui_Stuff.buttons buttons5;
+        private Ui_Stuff.buttons buttons6;
+    }
+}
+
